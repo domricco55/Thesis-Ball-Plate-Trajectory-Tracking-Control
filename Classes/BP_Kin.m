5ba3864715bf7dba5fd3c71e56e64d7863680327
Dominic Riccoboni <domricco55@gmail.com>
2021-08-14 22:27:25 -0700

    Started working on the ball-plate kinematics class.  Dont have much content yet but the shell is there. I need to think a bit about how I want to go about organizing the methods.

--- Classes/BP_Kin.m
+++ Classes/BP_Kin.m
@@ -1,31 +1,29 @@
-classdef UJ_Kin < handle
-    %UJ_Kin This class generates the 
+classdef BP_Kin < handle
+    %BP_Kin This class generates the kinematic equations for the motion of the ball
     %   Detailed explanation goes here
     
     properties
-        VDefs
         r0s
-        Theta_d_gam
-        Theta_d_beta
         Omega2ms
         Omega2vs
-        Omega_p
         v2s
         a2s
     end
-    
-    properties
+   
+    properties (SetAccess = private)
+        VDefs
+        UJ_Kin
+    end 
     
     methods
-        function obj = UJ_Kin(Var_Defs)
+        function obj = BP_Kin(Var_Defs, UJ_Kin)
             %UNTITLED Construct an instance of this class
             %   Detailed explanation goes here
             
             
-            %Assign the Var_Defs object that was in the constructor argument to the
-            %classes VDefs property
+            %Assign the inputs to the constructor to the class properties
             obj.VDefs = Var_Defs;          
-           
+            obj.UJ_Kin = UJ_Kin;
 
         end
         
@@ -51,16 +49,16 @@ classdef UJ_Kin < handle
             
             %Get the Theta_d_gam matrix by multiplying R12d by R21. R12d = Theta_D_gam*R12
             %and therefore R21 anihilates with R12.  
-            obj.Theta_d_gam = simplify(obj.VDefs.R12d*obj.VDefs.R21); %shows that if this is Theta_d_12 then Theta_d_12*R12 = R12d
+            Theta_d_gam = simplify(obj.VDefs.R12d*obj.VDefs.R21); %shows that if this is Theta_d_12 then Theta_d_12*R12 = R12d
             
             %Get the Theta_d_beta matrix by multiplying R01d by R10. R01d =
             %Theta_D_gam*R01 and therefore R10 anihilates with R01.  
-            obj.Theta_d_beta = simplify(obj.VDefs.R01d*obj.VDefs.R10);
+            Theta_d_beta = simplify(obj.VDefs.R01d*obj.VDefs.R10);
             
             %Derive the global angular velocity matrix of the plate frame in terms of beta
             %and gamma. This matrix has components expresseed in the S2 basis, the plate
             %frame. 
-            obj.Omega2ms = simplify((obj.Theta_d_gam + obj.VDefs.R12*obj.Theta_d_beta*obj.VDefs.R21).');
+            obj.Omega2ms = simplify((Theta_d_gam + obj.VDefs.R12*Theta_d_beta*obj.VDefs.R21).');
             
             %Extract the components of Omega2ms associated with Omega2vs, the vector
             %representation of the same information. 
@@ -75,8 +73,6 @@ classdef UJ_Kin < handle
             obj.v2s =  diff(obj.VDefs.r2s,obj.VDefs.t) + obj.Omega2ms*obj.VDefs.r2s; 
             
             %Return the outputs
-            Theta_d_gam = obj.Theta_d_gam;
-            Theta_d_beta = obj.Theta_d_beta;
             Omega2ms = obj.Omega2ms;
             Omega2vs = obj.Omega2vs;
             v2s = obj.v2s;
