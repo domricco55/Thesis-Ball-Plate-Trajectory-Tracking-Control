f1ceb52cce607318adfa807e59829da503c1c240
Dominic Riccoboni <domricco55@gmail.com>
2021-09-27 22:21:52 -0700

    Made more progress. I was able to get the simulilnk parameters generated by the Lin_Mvng_Stpt_Cntr_SS object but I'm not sure how those parameters will play into other simulink models if they are named the same. So, since I'm uncertain, I am going to make sure I reassign the parameters for any and all Simulink models IN THE CONTEXT OF A "Run_Sim' - LIKE FUNCTION CALL. So anytime I want to run a new simulation, I make sure all the parameters are recent and correct.

--- Classes/Lin_Mvng_Stpt_Cntr_SS.m
+++ Classes/Lin_Mvng_Stpt_Cntr_SS.m
@@ -4,7 +4,7 @@ classdef Lin_Mvng_Stpt_Cntr_SS < handle
     
     properties (SetAccess = private)
         
-        sys_matrices
+        sys_mats %System Matrices
         
     end
     
@@ -17,13 +17,23 @@ classdef Lin_Mvng_Stpt_Cntr_SS < handle
     
     methods
         function obj = Lin_Mvng_Stpt_Cntr_SS(Lnrzed_EOMs,type)
-            %UNTITLED Construct an instance of this class
+            %Lin_Mvng_Stpt_Cntr_SS Construct an instance of this class
             %   Detailed explanation goes here
             
             switch type
                 
                 case 'SS Integral Controller'
-                 
+                    
+                    %Augmented A Matrix
+                    obj.sys_mats.Aa = [zeros(5,1),[-1 0 0 0; Lnrzed_EOMs.A1]]; 
+                    
+                    %Augmented B Matrix
+                    obj.sys_mats.Ba = [0;Lnrzed_EOMs.B1];
+                    
+                    %S matrix, the setpoint matrix
+                    obj.sys_mats.Sa = sym([1;0;0;0;0]);
+                    
+
                 case 'SS PID Controller'    
                     
                 otherwise
@@ -33,15 +43,36 @@ classdef Lin_Mvng_Stpt_Cntr_SS < handle
         end
         
         
-        function sim_response = Run_Sim(obj,sp_symfun, tspan, sim_string)
+        function sim_response = Run_Sim(obj,sp_symfun, tspan, K)
             %METHOD1 Summary of this method goes here
             %   Detailed explanation goes here
+            
+            
+            %Bring in the gain matrix K
+            obj.K = K;
+            
+            %Set the Simulink Parameters for the matrices
+            Aa = Simulink.Parameter(double(obj.sys_mats.Aa));
+            assignin('base', 'Aa', Aa);
+
+            Ba = Simulink.Parameter(double(obj.sys_mats.Ba));
+            assignin('base', 'Ba', Ba);
+
+            Sa = Simulink.Parameter(double(obj.sys_mats.Sa));
+                    assignin('base', 'Sa', Sa);
+            
             sim_response = simout;
             
         end
         
         function [] = plot_results(obj, sim_results)
-            
+            %METHOD2 Summary of this method goes here
+            %   Detailed explanation goes here 
+        end 
+        
+        function [] = create_animation(obj, sim_results)
+            %METHOD3 Summary of this method goes here
+            %   Detailed explanation goes here
         end 
         
     end
