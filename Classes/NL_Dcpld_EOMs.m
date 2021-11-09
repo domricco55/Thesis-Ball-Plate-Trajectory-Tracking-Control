42de97c8e24fb834111e7c17ab25b11d43e165eb
Dominic Riccoboni <domricco55@gmail.com>
2021-11-08 21:40:00 -0800

    Got x-direction done and started on y-direction. Need to bring them both together at the end to get something I can use to generate a matlab function block in a Simulink model. May not ever use the decoupled model for Simulation but it will be good to have it if I need it.

--- Classes/NL_Dcpld_EOMs.m
+++ Classes/NL_Dcpld_EOMs.m
@@ -11,6 +11,10 @@ classdef NL_Dcpld_EOMs <handle
         SumMBall_x
         SumMPlate_x
         SolvedEOMs_x
+        g_1
+        f_1
+
+        %Numerical EOMs
 
     end
 
@@ -25,10 +29,10 @@ classdef NL_Dcpld_EOMs <handle
 
             %Decouple the x axis
 
-            %Ball Kinetics  x only
+            %Ball Kinetics x direction
             obj.SumMBall_x = simplify(subs(obj.BP_Kinetics.SumMBall(2),[obj.VDefs.gamma obj.VDefs.gamma_dot obj.VDefs.gamma_ddot ], [0 0 0]));
             
-            %Plate Kinetics x only
+            %Plate Kinetics x direction
             obj.SumMPlate_x = subs(obj.BP_Kinetics.SumMPlate(2),[obj.VDefs.gamma obj.VDefs.gamma_dot obj.VDefs.gamma_ddot], [0 0 0]);
 
             %Bring Together Nonlinear Decoupled Moment Equations and Solve for Highest Order Terms
@@ -36,11 +40,39 @@ classdef NL_Dcpld_EOMs <handle
             [CoeffMat_x,ExtTerms_x] = equationsToMatrix(EOMS_x , [obj.VDefs.x_ddot, obj.VDefs.beta_ddot]); 
             obj.SolvedEOMs_x = obj.VDefs.stateVec1_dot == [obj.VDefs.x_dot; [1 0]*(CoeffMat_x\ExtTerms_x); obj.VDefs.beta_dot;[0 1]*(CoeffMat_x\ExtTerms_x)];
 
-
+            %Find input matrix g(x) x direction
+            [obj.g_1,g_1_extra_terms] = equationsToMatrix(obj.SolvedEOMs_x , obj.VDefs.T_beta);
+            obj.g_1 = -obj.g_1;
+            g_1_extra_terms = -g_1_extra_terms;
+            
+            obj.f_1 = - g_1_extra_terms + obj.VDefs.stateVec1_dot;
         end
 
         function [] = Num_Params_n_Assumptions(obj)
-
+%         %Num_Params_n_Assumptions Input estimated numerical parameters of the system
+%             % and simplify the system with a few assumptions
+%             %   Detailed explanation goes here
+%             
+% 
+%             %Numerical Parameters in terms of symbolic variables utilized in 
+%             %the model 
+%             Num_Params = [obj.VDefs.rB  (obj.VDefs.rC+obj.VDefs.rB) ...
+%             obj.VDefs.mB obj.VDefs.mP obj.VDefs.IP obj.VDefs.IB ...
+%             obj.VDefs.g_num obj.VDefs.rG];
+%         
+%             Sym_Params = [obj.VDefs.r_b obj.VDefs.z_b obj.VDefs.m_b...
+%                 obj.VDefs.m_p obj.VDefs.I_p__xx obj.VDefs.I_b obj.VDefs.g...
+%                 obj.VDefs.z_p];
+%             
+%             %Simplifying assumptions
+%             Assumption_Params = [obj.VDefs.psi_dot_z obj.VDefs.psi_z obj.VDefs.I_p__zz];
+%             Assumptions = [0 0 0];
+%             
+%             %Substitute in all numerical parameters and assumptions
+%             obj.NL_NumEOMs = subs(obj.NL_EOMs, [Sym_Params Assumption_Params],[Num_Params  Assumptions]);
+%             
+%             %Return outputs
+%             NL_NumEOMs = obj.NL_NumEOMs;
 
         end
     end
